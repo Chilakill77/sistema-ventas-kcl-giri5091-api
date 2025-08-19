1d05123507e967206ae60f60ee847a897fff0c7e
Kevin Cabrera Luna <kev.cab3luna@gmail.com>
2025-08-19 11:44:55 -0600

    Chintrolas 6

---
+++ prisma/migrations/20250818214029_init/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Usuario` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `Usuario`;
+
+-- CreateTable
+CREATE TABLE `usuario` (
+    `cveUsuario` INTEGER NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(191) NOT NULL,
+    `apellidos` VARCHAR(191) NOT NULL,
+    `correo` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `fechaRegistro` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `Usuario_correo_key`(`correo`),
+    UNIQUE INDEX `Usuario_username_key`(`username`),
+    PRIMARY KEY (`cveUsuario`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
